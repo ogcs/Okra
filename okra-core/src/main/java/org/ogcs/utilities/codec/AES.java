c83687ae1dfd5b25f8c464575cff6eae217678ce
TinyZ <tinyzzh815@gmail.com>
2016-09-25 15:41:04 +0800

    Modify : 优化AES加密工具

---
+++ okra-core/src/main/java/org/ogcs/utilities/codec/AES.java
@@ -0,0 +1,93 @@
+/*
+ *     Copyright 2016-2026 TinyZ
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.ogcs.utilities.codec;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+/**
+ * AES Cipher Util.
+ *
+ * @since 2.0
+ */
+public final class AES {
+
+    private static final Logger LOG = LogManager.getLogger(AES.class);
+    private static final String AES = "AES";
+    private static String CIPHER_AES = "AES/CBC/PKCS5Padding";
+    private static byte[] DEFAULT_KEY = "DnA DoubleSpiral".getBytes();  //  16的整数倍
+    private static byte[] DEFAULT_IV = DEFAULT_KEY;
+
+    static {
+        CIPHER_AES = System.getProperty("codec.cipher.aes", CIPHER_AES);
+    }
+
+    private AES() {
+        //  no-op
+    }
+
+    // ENCRYPT
+
+    public static String encryptToBase64(byte[] data) {
+        return Base64.getEncoder().encodeToString(encrypt(data));
+    }
+
+    public static byte[] encrypt(byte[] data) {
+        return encrypt(CIPHER_AES, AES, data, DEFAULT_KEY, DEFAULT_IV);
+    }
+
+    public static byte[] encrypt(String cipherAes, String sks, byte[] data, byte[] key, byte[] iv) {
+        try {
+            Cipher cipher = Cipher.getInstance(cipherAes);
+            SecretKeySpec keySpec = new SecretKeySpec(key, sks);
+            IvParameterSpec ivSpec = new IvParameterSpec(iv);
+            cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivSpec);
+            return cipher.doFinal(data);
+        } catch (Exception e) {
+            LOG.warn("AES encrypt error: data : " + String.valueOf(data), e);
+            return null;
+        }
+    }
+
+    // DECRYPT
+
+    public static byte[] decryptFromBase64(String content) {
+        return decrypt(Base64.getDecoder().decode(content));
+    }
+
+    public static byte[] decrypt(byte[] data) {
+        return decrypt(CIPHER_AES, AES, data, DEFAULT_KEY, DEFAULT_IV);
+    }
+
+    public static byte[] decrypt(String cipherAes, String sks, byte[] data, byte[] key, byte[] iv) {
+        try {
+            Cipher cipher = Cipher.getInstance(cipherAes);
+            SecretKeySpec keySpec = new SecretKeySpec(key, sks);
+            IvParameterSpec ivSpec = new IvParameterSpec(iv);
+            cipher.init(Cipher.DECRYPT_MODE, keySpec, ivSpec);
+            return cipher.doFinal(data);
+        } catch (Exception e) {
+            LOG.warn("AES decrypt error: data : " + String.valueOf(data), e);
+            return null;
+        }
+    }
+}
--- okra-core/src/main/java/org/ogcs/utilities/codec/XXTEA.java
+++ okra-core/src/main/java/org/ogcs/utilities/codec/XXTEA.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.ogcs.utilities.security;
+package org.ogcs.utilities.codec;
 public final class XXTEA {
     private XXTEA() {}
 
